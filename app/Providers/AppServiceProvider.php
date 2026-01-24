0bd093d7f03a15106af24dff424475b183025ef7
Eduardo Henrique <eduardohbrasil@icloud.com>
2026-01-24 14:54:53 -0400

    chore(auth): install Socialite and IVAO provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,43 +5,50 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use SocialiteProviders\Ivao\Provider;
+use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        $this->configureDefaults();
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		$this->configureDefaults();
+	}
 
-    protected function configureDefaults(): void
-    {
-        Date::use(CarbonImmutable::class);
+	protected function configureDefaults(): void
+	{
+		Date::use(CarbonImmutable::class);
 
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
+		DB::prohibitDestructiveCommands(
+			app()->isProduction(),
+		);
 
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null
-        );
-    }
+		Password::defaults(fn(): ?Password => app()->isProduction()
+			? Password::min(12)
+				->mixedCase()
+				->letters()
+				->numbers()
+				->symbols()
+				->uncompromised()
+			: null
+		);
+
+		Event::listen(static function (SocialiteWasCalled $event) {
+			$event->extendSocialite('ivao', Provider::class);
+		});
+	}
 }
